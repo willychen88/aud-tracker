9b7455a2c04e8b1138a7fb12f559e266c2f2425f
willychen88 <84728156+willychen88@users.noreply.github.com>
2026-09-04 12:47:54 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+var CACHE_NAME = 'rate-tracker-v1';
+var urlsToCache = [
+  '/aud-tracker/',
+  '/aud-tracker/index.html',
+  '/aud-tracker/manifest.json',
+  '/aud-tracker/icon-192.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      // Network first, cache fallback
+      if (response && response.status === 200) {
+        var responseClone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseClone);
+        });
+      }
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
